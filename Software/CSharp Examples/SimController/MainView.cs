c77de5318cc2de6ff7c0f56eb73af780636a8edf
Caleb Chamberlain <calebc@oshcut.com>
2025-07-02 16:11:31 -0700

    Add UDP code

--- Software/CSharp Examples/SimController/MainView.cs
+++ Software/CSharp Examples/SimController/MainView.cs
@@ -2,15 +2,44 @@ namespace SimController
 {
     public partial class MainView : Form
     {
+        private readonly UdpReceiver _receiver;
+        private MotorInterface? motorInterface;
+
         public MainView()
         {
             InitializeComponent();
+
+            _receiver = new UdpReceiver(5123);
+            _receiver.MessageReceived += OnUdpMessageReceived;
+            _receiver.Start();
+        }
+
+        private void OnUdpMessageReceived(string message)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(() => UpdateLabel(message));
+            }
+            else
+            {
+                UpdateLabel(message);
+            }
+        }
+
+        private void UpdateLabel(string message)
+        {
+            motorComStatusLabel.Text = message;
         }
 
         private void estopButton_Click(object sender, EventArgs e)
         {
             // TODO: Stop all motion, disable all motors!
 
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            _receiver.Stop();
+            base.OnFormClosing(e);
         }
     }
 }
---
+++ Software/CSharp Examples/SimController/UdpReceiver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Concurrent;
+
+namespace SimController
+{
+    class UdpReceiver
+    {
+        private readonly int _port;
+        private readonly UdpClient _udpClient;
+        private readonly CancellationTokenSource _cts = new();
+
+        public event Action<string>? MessageReceived;
+
+        public UdpReceiver(int port)
+        {
+            _port = port;
+            _udpClient = new UdpClient(_port);
+        }
+
+        public void Start()
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    while (!_cts.Token.IsCancellationRequested)
+                    {
+                        var result = await _udpClient.ReceiveAsync();
+                        string message = Encoding.UTF8.GetString(result.Buffer);
+
+                        MessageReceived?.Invoke(message); // Raise event
+                    }
+                }
+                catch (ObjectDisposedException) { }
+            });
+        }
+
+        public void Stop()
+        {
+            _cts.Cancel();
+            _udpClient.Close();
+        }
+    }
+}
