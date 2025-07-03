55a7aa74fb11f6710dccc655dad5dec7616aff1c
Caleb Chamberlain <calebc@oshcut.com>
2025-07-03 12:29:59 -0700

    WIP

--- Software/CSharp Examples/SimController/MotorInterface.cs
+++ Software/CSharp Examples/SimController/MotorInterface.cs
@@ -12,49 +12,40 @@ namespace SimController
     {
         const int HOMING_TIMEOUT_MS = 60000;
 
-        private string lastStatus = "";
-        public string LastStatus => lastStatus;
-       
-        private bool pitchHomed = false;
-        public bool PitchHomed => pitchHomed;
+        private SimulatorState simulatorState;
 
-        private bool rollHomed = false;
-        public bool RollHomed => rollHomed;
-
-        private bool yawHomed = false;
-        public bool YawHomed => yawHomed;
-
-        private bool pitchMotorEnabled = false;
-        public bool PitchMotorEnabled => pitchMotorEnabled;
-        private bool rollMotorEnabled = false;
-        public bool RollMotorEnabled => rollMotorEnabled;
-        private bool yawMotorEnabled = false;
-        public bool YawMotorEnabled => yawMotorEnabled;
-
-        private cliSysMgr myMgr;
+        private cliSysMgr? myMgr;
         private string? comHubPort;
         private cliIPort? myPort;
-        private Boolean hubPortInitialized = false;
+        private cliINode[]? myNodes;
 
         public event Action<string>? StatusChanged;
+        public event Action<SimulatorState>? StateChanged;
         public readonly IProgress<string> StatusReporter;
-
+        public readonly IProgress<SimulatorState> StateReporter;
 
         public MotorInterface() {
-            //Create the SysManager object. This object will coordinate actions among various ports
-            // and within nodes. In this example we use this object to setup and open our port.
-            myMgr = new cliSysMgr();
             StatusReporter = new Progress<string>(msg => StatusChanged?.Invoke(msg));
+            StateReporter = new Progress<SimulatorState>(state => StateChanged?.Invoke(state));
+            simulatorState = new SimulatorState();
         }
 
         public void Start()
         {
             List<String> comHubPorts = new List<String>();
 
+            if (simulatorState.portConnected) {
+                throw new Exception("Attempting to initialize the motor interface, but it has already been initialized.");
+            }
+
             Task.Run(() =>
             {
                 try
                 {
+                    //Create the SysManager object. This object will coordinate actions among various ports
+                    // and within nodes. In this example we use this object to setup and open our port.
+                    myMgr = new cliSysMgr();
+
                     StatusReporter.Report("Connecting to motion simulator.");
                     myMgr.FindComHubPorts(comHubPorts);
 
@@ -69,8 +60,23 @@ namespace SimController
                     myMgr.PortsOpen(1);
                     myPort = myMgr.Ports(0);
 
-                    hubPortInitialized = true;
+                    if (myPort.NodeCount() != 3)
+                    {
+                        StatusReporter.Report("Error, expected 3 motors, found " + myPort.NodeCount() + ".");
+                        myMgr.PortsClose();
+                        myPort.Dispose();
+                        myMgr.Dispose();
+                        return;
+                    }
+                    myNodes = new cliINode[myPort.NodeCount()];
+                    for (int n = 0; n < myNodes.Length; n++)
+                    {
+                        myNodes[n] = myPort.Nodes(n);
+                    }
+
                     StatusReporter.Report("Connected.");
+                    simulatorState.portConnected = true;
+                    StateReporter.Report(simulatorState);
                 }
                 catch (Exception e)
                 {
@@ -82,105 +88,112 @@ namespace SimController
 
         public void ZeroAllMotors()
         {
-            if (hubPortInitialized && myPort != null)
+            Task.Run(() =>
             {
-                cliINode[] myNodes = new cliINode[myPort.NodeCount()];
-                Console.WriteLine("Port {0}: state={1}, nodes={2}", myPort.NetNumber(), myPort.OpenState(), myPort.NodeCount());
-
-                //Once the code gets past this point, it can be assumed that the Port has been opened without issue
-                //Now we can get a reference to our port object which we will use to access the node objects
-                for (int n = 0; n < myPort.NodeCount(); n++)
+                if (simulatorState.portConnected && myMgr != null && myPort != null && myNodes != null)
                 {
-                    // Create a shortcut reference for a node
-                    myNodes[n] = myPort.Nodes(n);
-                    myNodes[n].EnableReq(false);       //Ensure Node is disabled
-                    myMgr.Delay(200);
+                    Console.WriteLine("Port {0}: state={1}, nodes={2}", myPort.NetNumber(), myPort.OpenState(), myPort.NodeCount());
 
-                    Console.WriteLine("   Node[{0}]: type={1}", n, myNodes[n].Info.NodeType());
-                    Console.WriteLine("            userID: {0}", myNodes[n].Info.UserID);
-                    Console.WriteLine("        FW version: {0}", myNodes[n].Info.FirmwareVersion.Value());
-                    Console.WriteLine("          Serial #: {0}", myNodes[n].Info.SerialNumber.Value());
-                    Console.WriteLine("             Model: {0}", myNodes[n].Info.Model.Value());
+                    simulatorState.motorsEnabled = false;
+                    simulatorState.motorsHomed = false;
+                    simulatorState.homingInProgress = true;
+                    StateReporter.Report(simulatorState);
 
-                    // The following statements will attempt to enable the node.  First,
-                    // any shutdowns or NodeStops are cleared, finally the node is enabled
-                    myNodes[n].Status.AlertsClear();
-                    myNodes[n].Motion.NodeStopClear();
-                    myNodes[n].EnableReq(true);
-                    Console.WriteLine("Node {0} enabled.", n);
-                    double timeout = myMgr.TimeStampMsec() + HOMING_TIMEOUT_MS;     // Define a timeout in case the node is unable to enable
-                                                                                    // This will loop checking on the Real time values of the node's Ready status
-                    while (!myNodes[n].Motion.IsReady())
-                    {
-                        if (myMgr.TimeStampMsec() > timeout)
-                        {
-                            Console.WriteLine("Error: Timed out waiting for Node {0} to enable.", n);
+                    //Once the code gets past this point, it can be assumed that the Port has been opened without issue
+                    //Now we can get a reference to our port object which we will use to access the node objects
+                    for (int n = 0; n < myNodes.Length; n++)
+                    {                       
+                        myNodes[n].EnableReq(false);
+                        myMgr.Delay(200);
 
-                        }
-                    }
+                        Console.WriteLine("   Node[{0}]: type={1}", n, myNodes[n].Info.NodeType());
+                        Console.WriteLine("            userID: {0}", myNodes[n].Info.UserID);
+                        Console.WriteLine("        FW version: {0}", myNodes[n].Info.FirmwareVersion.Value());
+                        Console.WriteLine("          Serial #: {0}", myNodes[n].Info.SerialNumber.Value());
+                        Console.WriteLine("             Model: {0}", myNodes[n].Info.Model.Value());
 
-                    if (myNodes[n].Motion.Homing.HomingValid())
-                    {
-                        if (myNodes[n].Motion.Homing.WasHomed())
-                        {
-                            Console.WriteLine("Node {0} has already been homed, current position is: {1} ", n, myNodes[n].Motion.PosnMeasured.Value());
-                            Console.WriteLine("Rehoming Node... \n");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Node [{0}] has not been homed.  Homing Node now...", n);
-                        }
-                        // Now we will home the Node
-                        myNodes[n].Motion.Homing.Initiate();
-
-                        timeout = myMgr.TimeStampMsec() + HOMING_TIMEOUT_MS;    // Define a timeout in case the node is unable to enable
-                                                                                // Basic mode - Poll until disabled
-                        while (!myNodes[n].Motion.Homing.WasHomed())
+                        // The following statements will attempt to enable the node.  First,
+                        // any shutdowns or NodeStops are cleared, finally the node is enabled
+                        myNodes[n].Status.AlertsClear();
+                        myNodes[n].Motion.NodeStopClear();
+                        myNodes[n].EnableReq(true);
+                        Console.WriteLine("Node {0} enabled.", n);
+                        double timeout = myMgr.TimeStampMsec() + HOMING_TIMEOUT_MS;     // Define a timeout in case the node is unable to enable
+                                                                                        // This will loop checking on the Real time values of the node's Ready status
+                        while (!myNodes[n].Motion.IsReady())
                         {
                             if (myMgr.TimeStampMsec() > timeout)
                             {
-                                Console.WriteLine("Node did not complete homing:  \n\t -Ensure Homing settings have been defined through ClearView. \n\t -Check for alerts/Shutdowns \n\t -Ensure timeout is longer than the longest possible homing move.");
-
+                                Console.WriteLine("Error: Timed out waiting for Node {0} to enable.", n);
                             }
                         }
-                        myNodes[n].Motion.PosnMeasured.Refresh();      // Refresh our current measured position
-                        Console.WriteLine("Node completed homing, current position: {0} ", myNodes[n].Motion.PosnMeasured.Value());
-                        Console.WriteLine("Soft limits now active");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Node[{0}] has not had homing setup through ClearView.  The node will not be homed.", n);
-                    }
 
-                    // Disable the node
-                    Console.WriteLine("Disabling Node.");
-                    myNodes[n].EnableReq(false);
+                        if (myNodes[n].Motion.Homing.HomingValid())
+                        {
+                            if (myNodes[n].Motion.Homing.WasHomed())
+                            {
+                                Console.WriteLine("Node {0} has already been homed, current position is: {1} ", n, myNodes[n].Motion.PosnMeasured.Value());
+                                Console.WriteLine("Rehoming Node... \n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Node [{0}] has not been homed.  Homing Node now...", n);
+                            }
+                            // Now we will home the Node
+                            myNodes[n].Motion.Homing.Initiate();
 
-                    // This will dispose of the reference to the node. This frees up memory (similar to C++'s delete)
-                    // NOTE: All Teknic CLI classes implement the IDisposable pattern and should be properly disposed of when no longer in use.
-                    myNodes[n].Dispose();
+                            timeout = myMgr.TimeStampMsec() + HOMING_TIMEOUT_MS;    // Define a timeout in case the node is unable to enable
+                                                                                    // Basic mode - Poll until disabled
+                            while (!myNodes[n].Motion.Homing.WasHomed())
+                            {
+                                if (myMgr.TimeStampMsec() > timeout)
+                                {
+                                    Console.WriteLine("Node did not complete homing:  \n\t -Ensure Homing settings have been defined through ClearView. \n\t -Check for alerts/Shutdowns \n\t -Ensure timeout is longer than the longest possible homing move.");
+
+                                }
+                            }
+                            myNodes[n].Motion.PosnMeasured.Refresh();      // Refresh our current measured position
+                            Console.WriteLine("Node completed homing, current position: {0} ", myNodes[n].Motion.PosnMeasured.Value());
+                            Console.WriteLine("Soft limits now active");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Node[{0}] has not had homing setup through ClearView.  The node will not be homed.", n);
+                        }
+
+                        // Disable the node
+                        Console.WriteLine("Disabling Node.");
+                        myNodes[n].EnableReq(false);
+                    }
                 }
-            }
+            });
         }
 
         public void Dispose()
         {
-            // Disable all nodes (motors) and close the ports
-            cliIPort myPort = myMgr.Ports(0);
-            cliINode[] myNodes = new cliINode[myPort.NodeCount()];
-            Console.WriteLine("Port {0}: state={1}, nodes={2}", myPort.NetNumber(), myPort.OpenState(), myPort.NodeCount());
-
-            //Once the code gets past this point, it can be assumed that the Port has been opened without issue
-            //Now we can get a reference to our port object which we will use to access the node objects
-            for (int n = 0; n < myPort.NodeCount(); n++)
+            Task.Run(() =>
             {
-                // Create a shortcut reference for a node
-                myNodes[n] = myPort.Nodes(n);
-                myNodes[n].EnableReq(false);
-            }
-            myMgr?.PortsClose();
-            myPort?.Dispose();
-            myMgr?.Dispose();
+                // Disable all nodes (motors) and close the ports
+                if (myPort != null && myNodes != null)
+                {
+                    for (int n = 0; n < myPort.NodeCount(); n++)
+                    {
+                        // Create a shortcut reference for a node
+                        myNodes[n] = myPort.Nodes(n);
+                        myNodes[n].EnableReq(false);
+                        myNodes[n].Dispose();
+                    }
+                }
+
+                myMgr?.PortsClose();
+                myPort?.Dispose();
+                myMgr?.Dispose();
+
+                simulatorState.portConnected = false;
+                simulatorState.motorsEnabled = false;
+                simulatorState.motorsHomed = false;
+                StateReporter.Report(simulatorState);
+            });            
         }
     }
 }
---
+++ Software/CSharp Examples/SimController/SimulatorState.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimController
+{
+    internal class SimulatorState
+    {
+        public Boolean portConnected = false;
+        public Boolean motorsEnabled = false;
+        public Boolean motorsHomed = false;
+        public Boolean homingInProgress = false;
+
+        public long yawCounts = 0;
+        public long pitchCounts = 0;
+        public long rollCounts = 0;
+        public double yaw = 0;
+        public double pitch = 0;
+        public double roll = 0;
+
+        public double yawRate = 0;
+        public double pitchRate = 0;
+        public double rollRate = 0;
+
+        public double yawTorque = 0;
+        public double pitchTorque = 0;
+        public double rollTorque = 0;
+
+        public double yawCommandedRate = 0;
+        public double pitchCommandedRate = 0;
+        public double rollCommandedRate = 0;
+
+        public double yawCommandedPosition = 0;
+        public double pitchCommandedPosition = 0;
+        public double rollCommandedPosition = 0;
+
+        public long yawCountsError = 0;
+        public long pitchCountsError = 0;
+        public long rollCountsError = 0;
+
+        public SimulatorState()
+        {
+
+        }
+        public SimulatorState(SimulatorState state)
+        {
+            portConnected = state.portConnected;
+            motorsEnabled = state.motorsEnabled;
+            motorsHomed = state.motorsHomed;
+            homingInProgress = state.homingInProgress;
+
+            yawCounts = state.yawCounts;
+            pitchCounts = state.pitchCounts;
+            rollCounts = state.rollCounts;
+
+            yaw = state.yaw;
+            pitch = state.pitch;
+            roll = state.roll;
+
+            yawRate = state.yawRate;
+            pitchRate = state.pitchRate;
+            rollRate = state.rollRate;
+
+            yawTorque = state.yawTorque;
+            pitchTorque = state.pitchTorque;
+            rollTorque = state.rollTorque;
+
+            yawCommandedRate = state.yawCommandedRate;
+            pitchCommandedRate = state.pitchCommandedRate;
+            rollCommandedRate = state.rollCommandedRate;
+
+            yawCommandedPosition = state.yawCommandedPosition;
+            pitchCommandedPosition = state.pitchCommandedPosition;
+            rollCommandedPosition = state.rollCommandedPosition;
+
+            yawCountsError = state.yawCountsError;
+            pitchCountsError = state.pitchCountsError;
+            rollCountsError = state.rollCountsError;
+        }
+    }
+}
